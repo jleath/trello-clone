a363f7210f70e49feae3055e03d7ebb0ca3b3f0e
Joshua Leath <lawnfurniture@icloud.com>
2022-02-19 21:21:34 -0500

    splitting up react components

--- client/src/components/dashboard/SingleList.jsx
+++ client/src/components/dashboard/SingleList.jsx
@@ -1,19 +1,10 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import SingleCard from "./SingleCard";
+import ListDropdown from './ListDropdown';
+import Cards from './Cards';
+import NewCard from './NewCard';
 
 const SingleList = ({ list }) => {
-  const tempCards = useSelector(state => state.cards)
-  const cards = tempCards.filter(card => list._id === card.listId);
   const [ focus, setFocus ] = useState(false);
-  console.log('Cards: ', cards)
-
-  const generateListWrapperClass = () => {
-    if (focus) {
-      return "list-wrapper add-dropdown-active";
-    }
-    return "list-wrapper";
-  };
 
   const toggleFocus = e => {
     e.preventDefault();
@@ -21,56 +12,16 @@ const SingleList = ({ list }) => {
   };
 
   return (
-    <div className={generateListWrapperClass()}>
+    <div className={focus ? 'list-wrapper add-dropdown-active' : 'list-wrapper'}>
       <div className="list-background">
         <div className="list">
-          <a className="more-icon sm-icon" href=""></a>
-          <div>
-            <p className="list-title">{list.title}</p>
-          </div>
-          <div className="add-dropdown add-top">
-            <div className="card"></div>
-            <a className="button">Add</a>
-            <i className="x-icon icon"></i>
-            <div className="add-options">
-              <span>...</span>
-            </div>
-          </div>
-          <div id="cards-container" data-id="list-1-cards">
-            {cards.map(card => <SingleCard key={card._id} card={card} />)}
-          </div>
-          <div className="add-dropdown add-bottom active-card">
-            <div className="card">
-              <div className="card-info"></div>
-              <textarea name="add-card"></textarea>
-              <div className="members"></div>
-            </div>
-            <a
-              className="button"
-              onClick={toggleFocus}
-            >
-              Add
-            </a>
-            <i
-              className="x-icon icon"
-              onClick={toggleFocus}
-            >
-            </i>
-            <div className="add-options">
-              <span>...</span>
-            </div>
-          </div>
-          <div
-            className="add-card-toggle"
-            data-position="bottom"
-            onClick={toggleFocus}
-          >
-            Add a card...
-          </div>
+          <ListDropdown listTitle={list.title} />
+          <Cards listId={list._id}/>
+          <NewCard onFocusToggle={toggleFocus}/>  
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SingleList;
